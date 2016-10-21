3e3192a9231692e826b206c01ce9c7c14fa5ae63
Mohammed Shaik Hussain Ali <mindsmine@shaiksphere.com>
2016-10-21 02:42:20 -0700

    Converting to classes

--- src/helper/ObjectHelper.js
+++ src/helper/ObjectHelper.js
@@ -17,146 +17,141 @@
 /**
  * A collection of useful static methods to deal with JavaScript objects.
  *
- * @singleton
- *
- * @since 1.0
+ * @since 1.0.0
  *
  */
-mindsmine.Object = (function () {
+mindsmine.Object = class {
+    /**
+     * Freezes the given object making it immutable. This operation is by default shallow and does not affect objects
+     * referenced by the given object.
+     *
+     * @param {Object} obj The object to freeze.
+     * @param {Boolean} [deep=false] Pass <code>true</code> to freeze sub-objects recursively.
+     *
+     * @returns {Object} The given object <code>obj</code>.
+     *
+     * @since 1.0.0
+     *
+     */
+    static freeze(obj, deep) {
+        let self = this;
 
-    return {
-        /**
-         * Freezes the given object making it immutable. This operation is by default shallow and does not affect
-         * objects referenced by the given object.
-         *
-         * @param {Object} obj The object to freeze.
-         * @param {Boolean} [deep=false] Pass <code>true</code> to freeze sub-objects recursively.
-         *
-         * @returns {Object} The given object <code>obj</code>.
-         *
-         * @since 1.0
-         *
-         */
-        freeze: function (obj, deep) {
-            var self = this;
+        if (Object.freeze) {
+            if (obj && typeof obj === 'object' && !Object.isFrozen(obj)) {
+                Object.freeze(obj);
 
-            if (Object.freeze) {
-                if (obj && typeof obj === 'object' && !Object.isFrozen(obj)) {
-                    Object.freeze(obj);
-
-                    if (deep) {
-                        for (var name in obj) {
-                            if (obj.hasOwnProperty(name)) {
-                                self.freeze(obj[name], deep);
-                            }
+                if (deep) {
+                    for (let name in obj) {
+                        if (obj.hasOwnProperty(name)) {
+                            self.freeze(obj[name], deep);
                         }
                     }
                 }
             }
+        }
 
-            return obj;
-        },
+        return obj;
+    }
 
-        /**
-         * Returns the first matching key corresponding to the given value.
-         *
-         * Example usage:
-         *
-         *      var obj1 = { "key1" : "value1", "key2" : "value2"};
-         *
-         *      var val1 = "value2";
-         *
-         *      var key = mindsmine.Object.getKey(obj1, val1);
-         *
-         *      // key now contains the object: "key2"
-         *
-         * @param {Object} obj The object from which to retrieve the key.
-         *
-         * @param {Object} value The value to find.
-         *
-         * @returns {Object/null} First matching key. If no matching value is found, <code>null</code> is returned.
-         *
-         * @since 1.0
-         *
-         */
-        getKey: function (obj, value) {
-            if (obj && typeof obj === "object") {
-                for (var __prop in obj) {
-                    if (obj.hasOwnProperty(__prop) && obj[__prop] === value) {
-                        return __prop;
-                    }
+    /**
+     * Returns the first matching key corresponding to the given value.
+     *
+     * Example usage:
+     *
+     *      var obj1 = { "key1" : "value1", "key2" : "value2"};
+     *
+     *      var val1 = "value2";
+     *
+     *      var key = mindsmine.Object.getKey(obj1, val1);
+     *
+     *      // key now contains the object: "key2"
+     *
+     * @param {Object} obj The object from which to retrieve the key.
+     *
+     * @param {Object} value The value to find.
+     *
+     * @returns {Object/null} First matching key. If no matching value is found, <code>null</code> is returned.
+     *
+     * @since 1.0.0
+     *
+     */
+    static getKey(obj, value) {
+        if (obj && typeof obj === "object") {
+            for (let __prop in obj) {
+                if (obj.hasOwnProperty(__prop) && obj[__prop] === value) {
+                    return __prop;
                 }
             }
-
-            return null;
-        },
-
-        /**
-         * Copies the raw JSON object by value (instead of copy by reference).
-         *
-         * **NOTE**: Works on raw JSON objects alone.
-         *
-         * @param {Object} rawJSON To be copied by value.
-         *
-         * @returns {Object} Newly copied by value object.
-         *
-         * @since 1.0
-         *
-         */
-        copyRawJSON: function (rawJSON) {
-            if (rawJSON == null || typeof rawJSON !== "object") {
-                return rawJSON;
-            }
-
-            return JSON.parse(JSON.stringify(rawJSON));
-        },
-
-        /**
-         * Returns <code>true</code> if the passed value is a JavaScript primitive object - a string, number or boolean.
-         *
-         * @param {Object} value The value to test.
-         *
-         * @return {Boolean}
-         *
-         * @since 1.0
-         *
-         */
-        isPrimitive: function (value) {
-            var type = typeof value;
-
-            return type === 'string' || type === 'number' || type === 'boolean';
-        },
-
-        /**
-         * Returns a non-null object, even if the object being passed is a null object.
-         *
-         * If the passed-in object is a non-null object, then it is returned as-is.
-         *
-         * Example usage:
-         *
-         *      var obj1 = { "key1" : "value1", "key2" : "value2"};
-         *      var obj2 = null;
-         *
-         *      var obj3 = mindsmine.Object.getNullSafe(obj1);
-         *
-         *      var obj4 = mindsmine.Object.getNullSafe(obj2);
-         *
-         *      // obj3 now contains the object: { "key1" : "value1", "key2" : "value2"}
-         *      // obj4 now contains the object: {}
-         *
-         * @param {Object} obj The object to safeguard against <code>null</code>.
-         *
-         * @returns {Object} If obj is <code>null</code> then <code>{}</code> (empty object).
-         *
-         * @since 1.0
-         *
-         */
-        getNullSafe: function (obj) {
-            if (obj && typeof obj === "object") {
-                return obj;
-            }
-
-            return {};
         }
-    };
-})();
+
+        return null;
+    }
+
+    /**
+     * Copies the raw JSON object by value (instead of copy by reference).
+     *
+     * **NOTE**: Works on raw JSON objects alone.
+     *
+     * @param {Object} rawJSON To be copied by value.
+     *
+     * @returns {Object} Newly copied by value object.
+     *
+     * @since 1.0.0
+     *
+     */
+    static copyRawJSON(rawJSON) {
+        if (rawJSON == null || typeof rawJSON !== "object") {
+            return rawJSON;
+        }
+
+        return JSON.parse(JSON.stringify(rawJSON));
+    }
+
+    /**
+     * Returns <code>true</code> if the passed value is a JavaScript primitive object - a string, number or boolean.
+     *
+     * @param {Object} value The value to test.
+     *
+     * @return {Boolean}
+     *
+     * @since 1.0.0
+     *
+     */
+    static isPrimitive(value) {
+        let type = typeof value;
+
+        return type === 'string' || type === 'number' || type === 'boolean';
+    }
+
+    /**
+     * Returns a non-null object, even if the object being passed is a null object.
+     *
+     * If the passed-in object is a non-null object, then it is returned as-is.
+     *
+     * Example usage:
+     *
+     *      var obj1 = { "key1" : "value1", "key2" : "value2"};
+     *      var obj2 = null;
+     *
+     *      var obj3 = mindsmine.Object.getNullSafe(obj1);
+     *
+     *      var obj4 = mindsmine.Object.getNullSafe(obj2);
+     *
+     *      // obj3 now contains the object: { "key1" : "value1", "key2" : "value2"}
+     *      // obj4 now contains the object: {}
+     *
+     * @param {Object} obj The object to safeguard against <code>null</code>.
+     *
+     * @returns {Object} If obj is <code>null</code> then <code>{}</code> (empty object).
+     *
+     * @since 1.0.0
+     *
+     */
+    static getNullSafe(obj) {
+        if (obj && typeof obj === "object") {
+            return obj;
+        }
+
+        return {};
+    }
+};
