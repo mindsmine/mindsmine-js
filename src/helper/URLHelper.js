c5b53e9ba8152b733df9dabb40db4346155d0042
Mohammed Shaik Hussain Ali <me@shaiksphre.com>
2018-06-01 19:20:20 -0700

    Adding URL methods

---
+++ src/helper/URLHelper.js
@@ -0,0 +1,85 @@
+/*
+ Copyright 2008-present Shaiksphere, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+/**
+ * A collection of useful static methods to deal with JavaScript URLs.
+ *
+ * @since 3.2.0
+ *
+ */
+mindsmine.URL = class {
+    /**
+     * Returns <code>true</code> if the passed string is a valid URL, <code>false</code> otherwise.
+     *
+     * Example Usage:
+     *
+     *      mindsmine.URL.isValidURL("http://userid@example.com")       //  true
+     *      mindsmine.URL.isValidURL("www.example.com/main.html")       //  false
+     *
+     * @see {@link @MDN_API_URI@/URL|URL}
+     *
+     * @param {String} url to check
+     *
+     * @returns {Boolean} if the string is a valid URL
+     *
+     * @since 3.2.0
+     *
+     */
+    static isValidURL(url) {
+        if (mindsmine.String.isEmpty(url)) {
+            return false;
+        }
+
+        try {
+            let _url = new URL(url);
+        } catch (e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Appends content to the query string of a URL, handling logic for whether to place a question mark or ampersand.
+     *
+     * @see {@link @MDN_API_URI@/URL|URL}
+     * @see {@link @MDN_API_URI@/URLSearchParams|URLSearchParams}
+     *
+     * @param {String} url The URL to append to.
+     * @param {String} param The parameter key to append to the URL.
+     * @param {Object} value The parameter value to append to the URL.
+     *
+     * @returns {String} The resulting URL
+     *
+     * @since 3.2.0
+     *
+     */
+    static appendQuery(url, param, value) {
+        if (!this.isValidURL(url)) {
+            throw new TypeError("Fatal Error. 'url'. Invalid URL.");
+        }
+
+        if (mindsmine.String.isEmpty(param)) {
+            throw new TypeError("Fatal Error. 'param'. @ERROR_PERMITTED_STRING@");
+        }
+
+        let _url = new URL(url);
+
+        _url.searchParams.append(param, value);
+
+        return _url.href;
+    }
+};
---
+++ test/helper/URLHelper.test.js
@@ -0,0 +1,91 @@
+/*
+ Copyright 2008-present Shaiksphere, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+describe("isValidURL", () => {
+    test("should pass for valid URLs", () => {
+        [
+            "https://api.iextrading.com",
+            "https://api.iextrading.com/1.0/stock/market/batch",
+            "https://api.iextrading.com/1.0/stock/market/batch?types=quote",
+            "https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=AAPL",
+            "https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=AAPL#",
+            "https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=AAPL#tab1",
+            "http://userid:password@example.com:8080",
+            "http://userid:password@example.com:8080/",
+            "http://userid@example.com",
+            "http://userid@example.com/",
+            "http://userid@example.com:8080",
+            "http://userid@example.com:8080/",
+            "http://userid:password@example.com",
+            "http://userid:password@example.com/",
+            "http://142.42.1.1/",
+            "http://142.42.1.1:8080/",
+            "http://➡.ws/䨹",
+            "http://⌘.ws",
+            "http://⌘.ws/	",
+            "http://foo.com/blah_(wikipedia)#cite-1",
+            "http://foo.com/blah_(wikipedia)_blah#cite-1",
+            "http://foo.com/unicode_(✪)_in_parens",
+            "http://foo.com/(something)?after=parens",
+            "http://☺.damowmow.com/",
+            "http://code.google.com/events/#&product=browser",
+            "http://j.mp",
+            "ftp://foo.bar/baz",
+            "http://foo.bar/?q=Test%20URL-encoded%20stuff",
+            "http://مثال.إختبار	",
+            "http://例子.测试",
+        ].forEach(url => {
+            expect(mindsmine.URL.isValidURL(url)).toBeTruthy();
+        });
+    });
+
+    test("should fail for invalid URLs", () => {
+        [
+            "http://",
+            "http://?",
+            "http://??",
+            "http://??/",
+            "http://#",
+            "http://##",
+            "http://##/",
+            "//",
+            "//a",
+            "///a",
+            "foo.com",
+            "http:// shouldfail.com",
+            ":// should fail",
+            "/main.html",
+            "www.example.com/main.html"
+        ].forEach(url => {
+            expect(mindsmine.URL.isValidURL(url)).toBeFalsy();
+        });
+    });
+});
+
+test("appendQuery should append query params to the URL", () => {
+    const url = "http://www.google.com",
+        urlQ = `${url}?param1=value1`,
+        urlH = `${url}#hash1`,
+        urlQH = `${url}?param1=value1#hash1`,
+        param2 = "param2",
+        value2 = "value2",
+        query = "param2=value2";
+
+    expect(mindsmine.URL.appendQuery(url, param2, value2)).toBe("http://www.google.com/?param2=value2");
+    expect(mindsmine.URL.appendQuery(urlQ, param2, value2)).toBe("http://www.google.com/?param1=value1&param2=value2");
+    expect(mindsmine.URL.appendQuery(urlH, param2, value2)).toBe("http://www.google.com/?param2=value2#hash1");
+    expect(mindsmine.URL.appendQuery(urlQH, param2, value2)).toBe("http://www.google.com/?param1=value1&param2=value2#hash1");
+});
