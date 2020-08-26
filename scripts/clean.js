0dd14da52f010f620ffd0b895e23124876bf7ad9
Mohammed Shaik Hussain Ali <me@shaiksphre.com>
2020-08-25 22:44:04 -0400

    Making it ES6

--- scripts/clean.js
+++ scripts/clean.js
@@ -16,8 +16,8 @@
 
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const rmdir = function(dirPath) {
     if (fs.existsSync(dirPath)) {
