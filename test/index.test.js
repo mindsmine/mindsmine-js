df42a2492ed7a6c5594df2b4dd0d52ec8824f5f9
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2021-10-27 23:26:24 -0400

    Updating the versions

--- test/index.test.js
+++ test/index.test.js
@@ -21,7 +21,7 @@ test("mindsmine.productName should return product name", () => {
 });
 
 test("mindsmine.productVersion should return product version", () => {
-    expect(mindsmine.productVersion).toBe("4.5.1");
+    expect(mindsmine.productVersion).toBe("4.5.2");
 });
 
 const NOT_OBJECTS = [
