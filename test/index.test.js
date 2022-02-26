f9876c9737f26db40fcda9c5501e8fc858b6b0e7
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2022-02-25 22:59:58 -0500

    New version

--- test/index.test.js
+++ test/index.test.js
@@ -21,7 +21,7 @@ test("mindsmine.productName should return product name", () => {
 });
 
 test("mindsmine.productVersion should return product version", () => {
-    expect(mindsmine.productVersion).toBe("4.7.0");
+    expect(mindsmine.productVersion).toBe("4.8.0");
 });
 
 //_MORE_CODE
